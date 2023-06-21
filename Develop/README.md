0a04c0e54b6a9ff6d205d51686c2d41fd9aaf0c8
lugo.kse23@gmail.com <lugo.kse23@gmail.com>
2023-06-20 18:37:24 -0700

    javascript files complete

---
+++ Develop/README.md
@@ -0,0 +1,18 @@
+# gen read me 
+![Github license](https://img.shields.io/badge/license--blue.svg)
+## Description
+creates readme file based on user answers
+## Deployed Application URL
+not yet
+## Screenshot
+![alt-text](perhaps)
+## Table of Contents
+* [Features](#features)
+* [Languages & Dependencies](#languagesanddependencies)
+* [How to Use This Application](#HowtoUseThisApplication)
+## Features
+license badge
+## Languages & Dependencies
+inquirer
+## How to Use This Application:
+undefined
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,76 @@
-// TODO: Include packages needed for this application
+//Including packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//Creating an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What does your application do?.",
+  },
+  {
+    type: "input",
+    name: "screenshot",
+    message: "Do you have the relative path of the screenshot of your project?"
+  },
+  {
+    type: "input",
+    name: "link",
+    message: "Do you have a URL of your deployed application?"
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "What license would you like for your project?",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+  },
+  {
+    type: "input",
+    name: "require",
+    message: "Any dependencies?",
+  },
+  {
+    type: "input",
+    name: "features",
+    message: "What are some unique features of your project?",
+  },
+  {
+    type: "input",
+    name: "require",
+    message:
+      "What language(s) does your app use?",
+  },
+  {
+    type: "input",
+    name: "creator",
+    message: "Github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email address?",
+  }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Writing README.md File
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-// Function call to initialize app
+// Initializing app
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Creating Professional README.md File...");
+    writeToFile("README.md", generateMarkdown({ ...responses }));
+  });
+}
 init();
